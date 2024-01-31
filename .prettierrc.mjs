0a9b3f900c49f2f4417ec25f082077b21b2bd616
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-01-31 15:11:47 +0900

    rename prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,9 +1,9 @@
 /** @type {import("prettier").Config} */
 const config = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-};
+}
 
-export default config;
+export default config
