ee233482c417107607a0d5cd1dd07a92f53de3d2
foxytanuki <biz@foxytanuki.dev>
2024-04-04 23:09:45 +0900

    Fix env handling

--- packages/shared/lib/env.ts
+++ packages/shared/lib/env.ts
@@ -5,15 +5,7 @@ let prefix = ''
 let firebaseAPIKey = ''
 let firebaseProjectId = ''
 
-if (!!import.meta?.env) {
-  isDev = import.meta.env.DEV
-  apiURL = import.meta.env.VITE_API_COSMOS ?? 'http://localhost:1317'
-  rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? 'http://localhost:26657'
-  prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? 'mycel'
-  firebaseAPIKey = import.meta.env.VITE_FIREBASE_API_KEY ?? ''
-  firebaseProjectId =
-    import.meta.env.VITE_FIREBASE_PROJECT_ID ?? 'spore-testnet'
-} else {
+if (process.env.NEXT_RUNTIME === 'nodejs') {
   isDev = process.env.NODE_ENV !== 'production'
   apiURL = process.env.NEXT_PUBLIC_API_COSMOS ?? 'http://localhost:1317'
   rpcURL = process.env.NEXT_PUBLIC_WS_TENDERMINT ?? 'http://localhost:26657'
@@ -21,6 +13,14 @@ if (!!import.meta?.env) {
   firebaseAPIKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? ''
   firebaseProjectId =
     process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? 'spore-testnet'
+} else {
+  isDev = import.meta.env.DEV
+  apiURL = import.meta.env.VITE_API_COSMOS ?? 'http://localhost:1317'
+  rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? 'http://localhost:26657'
+  prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? 'mycel'
+  firebaseAPIKey = import.meta.env.VITE_FIREBASE_API_KEY ?? ''
+  firebaseProjectId =
+    import.meta.env.VITE_FIREBASE_PROJECT_ID ?? 'spore-testnet'
 }
 
 export const env = {
