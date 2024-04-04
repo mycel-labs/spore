84a247023d8365f700f69dc5ae29fe5830313714
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-04-04 11:49:38 +0900

    fix env

--- packages/shared/lib/env.ts
+++ packages/shared/lib/env.ts
@@ -1,19 +1,27 @@
-if (import.meta) {
-  const isDev = import.meta.env.DEV
-  const apiURL = import.meta.env.VITE_API_COSMOS ?? 'http://localhost:1317'
-  const rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? 'http://localhost:26657'
-  const prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? 'mycel'
-  const firebaseAPIKey = import.meta.env.VITE_FIREBASE_API_KEY ?? ''
-  const firebaseProjectId =
+let isDev = ''
+let apiURL = ''
+let rpcURL = ''
+let prefix = ''
+let firebaseAPIKey = ''
+let firebaseProjectId = ''
+
+if (import.meta.env) {
+  console.log('v')
+  isDev = import.meta.env.DEV
+  apiURL = import.meta.env.VITE_API_COSMOS ?? 'http://localhost:1317'
+  rpcURL = import.meta.env.VITE_WS_TENDERMINT ?? 'http://localhost:26657'
+  prefix = import.meta.env.VITE_ADDRESS_PREFIX ?? 'mycel'
+  firebaseAPIKey = import.meta.env.VITE_FIREBASE_API_KEY ?? ''
+  firebaseProjectId =
     import.meta.env.VITE_FIREBASE_PROJECT_ID ?? 'spore-testnet'
 } else {
-  const isDev = process.env.NODE_ENV !== 'production'
-  const apiURL = process.env.NEXT_PUBLIC_API_COSMOS ?? 'http://localhost:1317'
-  const rpcURL =
-    process.env.NEXT_PUBLIC_WS_TENDERMINT ?? 'http://localhost:26657'
-  const prefix = process.env.NEXT_PUBLIC_ADDRESS_PREFIX ?? 'mycel'
-  const firebaseAPIKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? ''
-  const firebaseProjectId =
+  console.log('n')
+  isDev = process.env.NODE_ENV !== 'production'
+  apiURL = process.env.NEXT_PUBLIC_API_COSMOS ?? 'http://localhost:1317'
+  rpcURL = process.env.NEXT_PUBLIC_WS_TENDERMINT ?? 'http://localhost:26657'
+  prefix = process.env.NEXT_PUBLIC_ADDRESS_PREFIX ?? 'mycel'
+  firebaseAPIKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? ''
+  firebaseProjectId =
     process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? 'spore-testnet'
 }
 
