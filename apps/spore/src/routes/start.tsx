a3d34434f552bf3d457f9e770261d0c9cb800de4
foxytanuki <biz@foxytanuki.dev>
2024-04-22 16:45:12 +0900

    Fix issue: not connected user can enter /home

--- apps/spore/src/routes/start.tsx
+++ apps/spore/src/routes/start.tsx
@@ -5,8 +5,9 @@ import { useStore } from '@/store'
 export const Route = createFileRoute('/start')({
   beforeLoad: async () => {
     const mycelName = useStore.getState().mycelName
+    const isConnected: boolean = useStore.getState().isConnected
     const user: boolean = await isReferralUserExist(mycelName)
-    if (user) {
+    if (isConnected && user) {
       throw redirect({ to: '/home' })
     }
   },
